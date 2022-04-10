0516dcf6e3b098844193036723c63d411559479b
chandra <chandra@example.com>
2022-04-10 08:53:29 +0545

    few changes

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +7,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent implements OnInit {
+public signup:any;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.signup = new FormGroup({
+      firstname: new FormControl('', [Validators.required,Validators.minLength(5)]),
+      lastname: new FormControl(),
+      Mobilenumber: new FormControl(),
+      email: new FormControl('', [Validators.required,Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.pattern('((?=.*?[0-9])(?=.*?[@!$*])(?=.*[a-z])(?=.*[A-Z]).{8,})')]),
+      Confirmpassword: new FormControl('', [Validators.required]),
+      DateOfBirth: new FormControl(),
+    })
   }
+  get firstname() {
+    return this.signup.get('firstname');
+  }
+
+  get lastname() {
+    return this.signup.get('lastname');
+  }
+
+  get Mobilenumber() {
+    return this.signup.get('email');
+  }
+  get email() {
+    return this.signup.get('firstname');
+  }
+  get password() {
+    return this.signup.get('firstname');
+  }
+  get Confirmpassword() {
+    return this.signup.get('firstname');
+  }
+  get DateOfBirth() {
+    return this.signup.get('firstname');
+  }
+
+  onSubmit(){
+    console.log(this.signup);
+    this.signup.reset();
+}
 
 }
